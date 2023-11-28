893435e84fe3eb38d886e43703cf64750776ee87
Marcelina <celisia25@op.pl>
2023-11-28 06:41:57 +0100

    Created solution

---
+++ src/DiceTest.java
@@ -0,0 +1,13 @@
+public class DiceTest {
+    public static void main(String[] args) {
+
+        Dice dice = new Dice(5);
+
+        dice.printInfo();
+        dice.roll();
+        dice.printInfo();
+
+
+    }
+
+}
